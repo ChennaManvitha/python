bf988b934c1798498ac96a081afdc9e8263761aa
ChennaManvitha <manvithachenna09@gmail.com>
2024-08-05 13:10:33 -0500

    adding changes

--- assignments/function_def.py
+++ assignments/function_def.py
@@ -14,19 +14,4 @@ def add(num1,num2):
     return(num3)
 
 add(3,2)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 print('add')
-=======
-print('add')
->>>>>>> 237385c (rebase with develop)
-=======
->>>>>>> f4b70bc (adding changes)
-=======
-print('add')
->>>>>>> 21e2202 (adding change)
-=======
-print('add')
->>>>>>> 21e22022ea950d8a6fcb746ac1aa4936680815d6
